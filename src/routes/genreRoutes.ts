85ac8e48b5d2c7721787479143f9da536a2e85d8
marshadwi <lucyanamarshadwi@gmail.com>
2025-02-03 12:45:20 +0700

    update

--- src/routes/genreRoutes.ts
+++ src/routes/genreRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createGenre, deleteGenre, getAllGenres, updateGenre } from "../controller/genreController"
+import { createGenre, deleteGenre, getAllGenres, getGenreById, updateGenre } from "../controller/genreController"
 
 const genreRouter = Router();
 
 genreRouter.get("/", getAllGenres);
 genreRouter.post("/", createGenre);
-genreRouter.get("/:id", getAllGenres);
+genreRouter.get("/:id", getGenreById);
 genreRouter.patch("/:id", updateGenre);
 genreRouter.delete("/:id", deleteGenre);
 
